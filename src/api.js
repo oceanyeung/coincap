f8671ecd293b4ccc882a7a54406b590b37d5ffbd
Ocean Yeung <oceanocean@gmail.com>
2020-08-11 03:56:03 -0700

    Added initial coins display

--- src/api.js
+++ src/api.js
@@ -1,2 +1,6 @@
 export default class Api {
+    static loadCoins({pagesize, pagenum, currency}) {
+        let url = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=${pagesize}&page=${pagenum}&sparkline=true`;
+        return fetch(url);
+    }
 }
--- src/main.js
+++ src/main.js
@@ -1,7 +1,17 @@
-import Page from './pageutils.js';
+import Page from './page.js';
 import Coins from './coins.js';
+import Api from './api.js';
 
 $(document).ready(() => {
     let params = Page.processUrl(window.location.href);
     console.log(params);
+
+    let pageButtons = Page.generatePager(params);
+    $("#pager").append(pageButtons);
+
+    Api.loadCoins(params).then(resp => {
+        resp.json().then(data => {
+            console.log(Coins.createCoinTable($("#coins"), data, params));
+        });
+    });
 });
